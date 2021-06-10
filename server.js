dbed229ce23fbeda05cb53c6cb908f10f7835372
Smith Scarborough <smithscarborough@gmail.com>
2021-06-10 15:18:02 -0500

    add try...catch blocks to express routes

--- server.js
+++ server.js
@@ -12,24 +12,36 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false })); // this tells the app that we are using URL parameters
 
 app.get('/', async (req, res) => {
-    const shortUrls = await ShortUrl.find()
-    res.render('index', { shortUrls: shortUrls });
+    try {
+        const shortUrls = await ShortUrl.find()
+        res.render('index', { shortUrls: shortUrls });
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 app.post('/shortUrls', async (req, res) => {
     // ShortUrl is what we named our db object, so ShortUrl.create is how we create a new short url
-    await ShortUrl.create({ full: req.body.fullUrl }) // note: the fullUrl value here on req.body.fullUrl is just what we named the input on the form in index.ejs
-    res.redirect('/');
+    try {
+        await ShortUrl.create({ full: req.body.fullUrl }) // note: the fullUrl value here on req.body.fullUrl is just what we named the input on the form in index.ejs
+        res.redirect('/');
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 app.get('/:shortUrl', async (req, res) => {
-    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
-    if (shortUrl == null) return res.sendStatus(404);
-
-    shortUrl.clicks++
-    shortUrl.save()
-
-    res.redirect(shortUrl.full);
-})
+    try {
+        const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+        if (shortUrl == null) return res.sendStatus(404);
+    
+        shortUrl.clicks++
+        shortUrl.save()
+    
+        res.redirect(shortUrl.full);
+    } catch (error) {
+        console.log(error);
+    } 
+});
 
 app.listen(process.env.PORT || 5000);
